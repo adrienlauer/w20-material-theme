8562e1398a2634ca021de2748ac78d4f49421388
Jean-Baptiste Lenglet <jbaptiste.lenglet+git@gmail.com>
2016-03-01 09:35:50 +0100

    Added Gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var gulp = require('gulp'),
+    bump = require('gulp-bump');
+ 
+gulp.task('bump', function() {
+  var typeIndex = process.argv.indexOf("--type") + 1,
+      type = typeIndex? process.argv[typeIndex]: 'patch';
+
+  gulp.src(["./package.json", "./bower.json"])
+    .pipe(bump({type: type}))
+    .pipe(gulp.dest('./'));
+});
+
+gul.task('test', function() {
+
+});
