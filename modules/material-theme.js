47fda2c102ee86adfed92ac319021e2323d9e97d
Jean-Baptiste Lenglet <jbaptiste.lenglet+git@gmail.com>
2016-03-04 13:11:20 +0100

    Moved RouteService in material-theme.js

--- modules/material-theme.js
+++ modules/material-theme.js
@@ -8,8 +8,6 @@ define([
     '[text]!{w20-material-theme}/templates/topbar.html',
     '[text]!{w20-material-theme}/templates/sidenav.html',
 
-    '{w20-material-theme}/modules/services/route-service',
-
     '{w20-core}/modules/culture',
     '{w20-core}/modules/utils',
 
@@ -140,6 +138,43 @@ define([
             };
         }
     ]);
+
+    w20MaterialTheme.factory('RouteService', ['$route', 'SecurityExpressionService', function($route, securityExpressionService) {
+
+        return {
+            routeCategories: categories,
+            routesFromCategory: route,
+            topLevelRoutes: route.bind(this, '__top'),
+            routeSortKey: sortKey,
+            isRouteVisible: isVisible
+        };
+
+        function isVisible(route) {
+            return !route.hidden && (typeof route.security === 'undefined' || securityExpressionService.evaluate(route.security));
+        }
+
+        function categories() {
+            return _.sortBy(_.uniq(_.filter(_.map($route.routes, function (route) {
+                if (angular.isDefined(route.category) && route.category !== '__top' && isVisible(route))
+                    return route.category;
+                return null;
+            }), function (elt) {
+                return elt !== null && (angular.isDefined(_config.categories) ? _.contains(_config.categories, elt) : true);
+            })), function (elt) {
+                return angular.isDefined(_config.categories)? _.indexOf(_config.categories, elt): elt;
+            });
+        }
+
+        function route(category) {
+            return _.filter($route.routes, function (route) {
+                return route.category === category && isVisible(route);
+            });
+        }
+
+        function sortKey(route) {
+            return route.sortKey || route.path
+        }
+    }]);
 
     return {
         angularModules: ['w20MaterialTheme'],
