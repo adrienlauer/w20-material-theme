d592e074393f2e3e20d85a4d08804e255dcd2561
Jean-Baptiste Lenglet <jbaptiste.lenglet+git@gmail.com>
2016-03-21 16:45:11 +0100

    change scope property

--- modules/module.js
+++ modules/module.js
@@ -25,19 +25,13 @@ define([
             return {
                 template: topbarTemplate,
                 restrict: 'E',
-                scope: true,
-                compile: compile
+                scope: {
+                    searchMaxlength: "@",
+                    searchPlaceholder: "@",
+                    searchDisabled: "<?"
+                },
+                link: link
             };
-
-            function compile(tElement, tAttrs) {
-
-                if(angular.isNumber(parseInt(tAttrs.searchMaxlength)) && !isNaN(parseInt(tAttrs.searchMaxlength)))
-                    tElement.find('input').attr('ng-maxlength', tAttrs.searchMaxlength);
-
-
-
-                return link;
-            }
 
             function link(scope, iElement, iAttrs) {
 
@@ -54,7 +48,7 @@ define([
                 scope.search = {
                     opened: false,
                     value: "",
-                    disabled: !!iAttrs.searchDisabled,
+                    disabled: false,
                     _placeholder: "",
                     get placeholder() {
                         return scope.search._placeholder;
@@ -84,13 +78,18 @@ define([
                     }
                 };
 
-                iAttrs.$observe("search-placeholder", function(value){
-                    scope.search.placeholder = formatPlaceholder(value);
+                scope.$watch(function() {
+                    return scope.searchDisabled;
+                }, function(newV, oldV) {
+                    if(newV !== oldV)
+                        scope.search.disabled = newV == "true" || newV == ""? true: false;
                 });
-                scope.search.placeholder = iAttrs.searchPlaceholder || "";
 
-                iAttrs.$observe("search-disabled", function(value) {
-                    scope.search.disabled = value == "false"? false: true;
+                scope.$watch(function() {
+                    return scope.searchPlaceholder;
+                }, function(newV, oldV) {
+                    if(newV !== oldV)
+                        scope.search.placeholder = newV;
                 });
 
                 scope.unregister = {
