e58681e721995d2a1b2f5cdba9a32994a69688bd
Jean-Baptiste Lenglet <jbaptiste.lenglet+git@gmail.com>
2016-04-13 13:52:02 +0200

    Updated README and gulpfile.js

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 # w20-material-theme
 W20 theme using Angular Material as the main styling library.
 
-## Installation
+## Start up
+
+### Prerequisites
+
+This project requires [NodeJS](https://nodejs.org/en/), [Node Package Manager (NPM)](https://www.npmjs.com/) (normally comes with NodeJS), [Bower](http://bower.io), ang [Gulp](http://gulpjs.com/).
+
+To install Bower and Gulp through NPM:
+
+```
+npm install -g bower
+npm install -g gulp-cli
+```
+NPM will install Bower and Gulp globally, so you can reuse them in other projects !
 
 ```
 git clone -b demo https://github.com/Magador/w20-material-theme.git
@@ -10,10 +22,11 @@ npm install
 bower install
 ```
 
-## Run demo
+### Run the proof of concept
 
 ```
-gulp serve
+gulp
 ```
 
-Then go to [http://localhost:3000](http://localhost:3000)
+It will serve the app through `browser-sync` then open a new tab with the default OS Internet app to [`http://localhost:3000`](http://localhost:3000)
+Each time a `.css`, `.html`, `.js`, `.json` or images change, the tab reloads.
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,38 @@
 var gulp = require('gulp');
 var serve = require('gulp-serve');
- 
-gulp.task('serve', serve('.'));
+var open = require('gulp-open');
+var browserSync = require('browser-sync');
+var historyApiFallback = require('connect-history-api-fallback');
+var reload = browserSync.reload;
+
+gulp.task('serve', function() {
+  browserSync({
+    port: 3000,
+    notify: false,
+    logPrefix: 'w20',
+    snippetOptions: {
+      rule: {
+        match: '<span id="browser-sync-binding"></span>',
+        fn: function(snippet) {
+          return snippet;
+        }
+      }
+    },
+    // Run as an https by uncommenting 'https: true'
+    // Note: this uses an unsigned certificate which on first access
+    //       will present a certificate warning in the browser.
+    // https: true,
+    server: {
+      baseDir: ['.'],
+      middleware: [historyApiFallback()]
+    }
+  });
+
+  gulp.watch(['w20-material-theme-demo/views/**/*.html', 'index.html'], reload);
+  gulp.watch(['w20-material-theme-demo/style/**/*.css'], reload);
+  gulp.watch(['w20-material-theme-demo/modules/**/*.js'], reload);
+  gulp.watch(['w20-material-theme-demo/images/**/*'], reload);
+  gulp.watch(['w20.app.json', 'w20-material-theme-demo/w20-material-theme-demo.w20.json'], reload);
+});
+
+gulp.task('default', ['serve'])
