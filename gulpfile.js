c72347cb79c36f8ff4e02e38d31c32b9079b4b3d
Jean-Baptiste Lenglet <jbaptiste.lenglet+git@gmail.com>
2016-05-02 10:59:58 +0200

    Add CSS lint and fixed css

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@
 var gulp = require('gulp');
 var bump = require('gulp-bump');
 var jshint = require('gulp-jshint');
+var csslint = require('gulp-csslint');
  
 gulp.task('bump', function() {
   var typeIndex = process.argv.indexOf('--type') + 1,
@@ -14,10 +15,18 @@ gulp.task('bump', function() {
     .pipe(gulp.dest('./'));
 });
 
-gulp.task('lint', function() {
+gulp.task('jshint', function() {
 	return gulp.src('modules/*.js')
 		.pipe(jshint())
 		.pipe(jshint.reporter('default'));
 });
+
+gulp.task('csslint', function() {
+	return gulp.src('style/*.css')
+		.pipe(csslint())
+		.pipe(csslint.reporter());
+});
+
+gulp.task('lint', ['jshint', 'csslint']);
 
 gulp.task('test', ['lint']);
