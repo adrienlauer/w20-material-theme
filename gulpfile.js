6d4fe3cf06666d04e6e259e4fcfcd35ba04cca25
Jean-Baptiste Lenglet <jbaptiste.lenglet+git@gmail.com>
2016-05-02 10:15:41 +0200

    JShint gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
-'use strict';
+/* jshint strict: false */
+/* globals process */
 
 var gulp = require('gulp');
 var bump = require('gulp-bump');
 var jshint = require('gulp-jshint');
  
 gulp.task('bump', function() {
-  var typeIndex = process.argv.indexOf("--type") + 1,
+  var typeIndex = process.argv.indexOf('--type') + 1,
       type = typeIndex? process.argv[typeIndex]: 'patch';
 
-  gulp.src(["./package.json", "./bower.json"])
+  gulp.src(['./package.json', './bower.json'])
     .pipe(bump({type: type}))
     .pipe(gulp.dest('./'));
 });
